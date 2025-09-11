d5e1ea5cf44861aef096b634474462b9a5d9a695
taiyebaafrin <taiyebasadia753@gmail.com>
2025-09-11 21:39:51 +0600

    d

--- phibook/settings.py
+++ phibook/settings.py
@@ -187,10 +187,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 #     'https://*.onrender.com',
 # ]
 
-# # Cookie settings for HTTPS
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
+# Cookie settings for HTTPS
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
-# # Security settings for production
-# SECURE_SSL_REDIRECT = True
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Security settings for production
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
